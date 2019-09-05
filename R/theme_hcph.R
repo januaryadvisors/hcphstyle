8fe8800867c89bae77114ec926004424be46f3f6
davidmcclendon <davidmmcclendon@gmail.com>
2019-09-05 08:39:10 -0500

    Reduce font size

--- R/theme_hcph.R
+++ R/theme_hcph.R
@@ -22,10 +22,10 @@ theme_hcph <- function () {
   ggplot2::theme(
     #Text format:
     #This sets the font, size, type and colour of text for the chart's title
-    plot.title = ggplot2::element_text(family = font,size = 32, face = "bold", color = "#081F30"),
+    plot.title = ggplot2::element_text(family = font,size = 20, face = "bold", color = "#081F30"),
 
     #This sets the font, size, type and colour of text for the chart's subtitle, as well as setting a margin between the title and the subtitle
-    plot.subtitle = ggplot2::element_text(family = font, size = 22, margin = ggplot2::margin(9, 0, 9, 0)),
+    plot.subtitle = ggplot2::element_text(family = font, size = 12, margin = ggplot2::margin(9, 0, 9, 0)),
 
     #This leaves the caption text element empty, because it is set elsewhere in the finalise plot function
     plot.caption = ggplot2::element_blank(),
@@ -37,12 +37,12 @@ theme_hcph <- function () {
     legend.background = ggplot2::element_blank(),
     legend.title = ggplot2::element_blank(),
     legend.key = ggplot2::element_blank(),
-    legend.text = ggplot2::element_text(family = font, size = 18, color = "#081F30"),
+    legend.text = ggplot2::element_text(family = font, size = 10, color = "#081F30"),
 
     #Axis format
     #This sets the text font, size and colour for the axis test, as well as setting the margins and removes lines and ticks. In some cases, axis lines and axis ticks are things we would want to have in the chart - the cookbook shows examples of how to do so.
-    axis.title = ggplot2::element_text(family = font, size = 18, color = "#081F30", face="bold"),
-    axis.text = ggplot2::element_text(family = font, size = 18, color = "#081F30"),
+    axis.title = ggplot2::element_text(family = font, size = 10, color = "#081F30", face="bold"),
+    axis.text = ggplot2::element_text(family = font, size = 10, color = "#081F30"),
     axis.text.x = ggplot2::element_text(margin = ggplot2::margin(5, b = 10)),
     axis.ticks = ggplot2::element_blank(),
     axis.line = ggplot2::element_blank(),
@@ -58,7 +58,7 @@ theme_hcph <- function () {
 
     #Strip background (#This sets the panel background for facet-wrapped plots to white, removing the standard grey ggplot background colour and sets the title size of the facet-wrap title to font size 22)
     strip.background = ggplot2::element_rect(fill = "white"),
-    strip.text = ggplot2::element_text(size = 22, hjust = 0)
+    strip.text = ggplot2::element_text(size = 10, hjust = 0)
   )
 }
 
